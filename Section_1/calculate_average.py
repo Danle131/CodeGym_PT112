c2cb722d01cb56e067fcb3dff9412c74b0a14e6e
Danle131 <143267337+Danle131@users.noreply.github.com>
2025-08-09 01:11:37 +0700

    Delete Section_1 directory

--- Section_1/calculate_average.py
+++
@@ -1,5 +0,0 @@
-num1 = input("Num1: ")
-num2 = input("Num2: ")
-num3 = input("Num3: ")
-average = (float(num1) + float(num2) + float(num3))/3
-print(f'{average:.5f}') 
--- Section_1/personal_info.py
+++
@@ -1,8 +0,0 @@
-name = input("Please enter your name: ")
-age = input("Please enter your age: ")
-address = input("Please enter your address: ")
-
-print(f'\n{"Personal Information":-^40}')
-print(f'Name    : {name}')
-print(f'Age     : {age}')
-print(f'Address : {address}')
