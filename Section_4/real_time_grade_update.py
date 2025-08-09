5101f69bc8efadc0ca9808f8bcf5559dc3d88993
Danle131 <143267337+Danle131@users.noreply.github.com>
2025-08-09 20:24:57 +0700

    Create real_time_grade_update.py

---
+++ Section_4/real_time_grade_update.py
@@ -0,0 +1,7 @@
+# Một học sinh có điểm ban đầu là 7.0. Sau khi được cộng thêm 1.5 điểm thưởng, rồi bị trừ 0.5 điểm kỷ luật. Hãy tính điểm cuối cùng.
+
+grade = 7.0
+grade += 1.5
+grade -= 0.5
+
+print(f'Final grade: {grade:.1f}')
