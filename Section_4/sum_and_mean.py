377dc53b0b01e6fb86a7973d9fbabd72ae098106
Danle131 <143267337+Danle131@users.noreply.github.com>
2025-08-09 20:20:04 +0700

    Create sum_and_mean.py

---
+++ Section_4/sum_and_mean.py
@@ -0,0 +1,7 @@
+num_1 = float(input("Please type value 1: "))
+num_2 = float(input("Please type value 2: "))
+num_3 = float(input("Please type value 3: "))
+
+total = float(num_1 + num_2 + num_3)
+
+print(f'Sum:  {total:.5f}\nMean: {total/3:.5f}')
