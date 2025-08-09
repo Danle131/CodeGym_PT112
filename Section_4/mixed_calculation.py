f5ee31ed4f94e8daba62da75df195092fd2f0522
Danle131 <143267337+Danle131@users.noreply.github.com>
2025-08-09 20:30:17 +0700

    Create mixed_calculation.py

---
+++ Section_4/mixed_calculation.py
@@ -0,0 +1,10 @@
+# Nhập các giá trị a, b, c và tính giá trị biểu thức sau:
+# KQ = (a + b) * c - a ** 2 // (b + 1)
+
+a = float(input("Please type value a: "))
+b = float(input("Please type value b: "))
+c = float(input("Please type value c: "))
+
+Value = float((a+b)*c-a**2//(b+1))
+
+print(f'Value: {Value:.5f}')
