f93672eb6e09cbd9476d6759f20fcf283a6b9e52
Danle131 <143267337+Danle131@users.noreply.github.com>
2025-08-19 21:17:58 +0700

    Update and rename xep_loai.py to xep_loai_if_elif.py

--- Section_5/xep_loai_if_elif.py
+++ Section_5/xep_loai_if_elif.py
@@ -6,7 +6,7 @@
 # Điểm < 5.0: Yếu
 
 diem_str = input("Nhập điểm trung bình của sinh viên (0 - 10): ")
-
+ # if elif
 if diem_str.isdigit():
     diem = float(diem_str)
     if 0 <= diem <= 10:
@@ -22,23 +22,3 @@ if diem_str.isdigit():
         print("Điểm nằm ngoài khoảng 0–10")
 else:
     print("Giá trị nhập không hợp lệ (phải là số)")
-
-
-# match case
-if diem_str.isdigit():
-    diem = float(diem_str)
-    if 0 <= diem <= 10:
-        match diem:
-            case d if d < 5:
-                print("Yếu")
-            case d if d < 7:
-                print("Trung bình")
-            case d if d < 8.5:
-                print("Khá")
-            case _:
-                print("Giỏi")
-    else:
-        print("Điểm nằm ngoài khoảng 0–10")
-else:
-    print("Giá trị nhập không hợp lệ (phải là số)")
-
