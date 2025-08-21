b967391549f432217b9f9b90dee762ddecd89fc8
Danle131 <143267337+Danle131@users.noreply.github.com>
2025-08-21 20:37:06 +0700

    Create ve_tam_giac.py

---
+++ Section_7/ve_tam_giac.py
@@ -0,0 +1,37 @@
+# Viết chương trình vẽ tam giác
+height = input("Nhập chiều cao tam giác: ")
+
+while height.isdigit():
+    x = int(height)
+
+    # Vẽ tam giác ver1
+    for i in range (1, x + 1):
+        for j in range (i):
+            print("#", end = "")
+        print()  
+
+    print()
+
+    # Vẽ tam giác ver2
+    for i in range(x, -1, -1):
+        for j in range (i):
+            print("#", end = "")
+        print()      
+
+    print()
+
+    # Vẽ tam giác ver3
+    step = 1
+    for i in range(x, 2*x):
+        j = 1
+        while (j <= x - step):
+            print(" ", end = "")
+            j += 1
+        for l in range(j, i + 1):
+                print("#", end = "")
+        print()
+        step+=1
+        j = 1
+    break
+
+
