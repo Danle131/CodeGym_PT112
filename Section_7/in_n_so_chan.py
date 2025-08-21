b7bf734a4cbe3ca5b232dfe6808c1f0592135e32
Danle131 <143267337+Danle131@users.noreply.github.com>
2025-08-21 21:32:29 +0700

    Create in_n_so_chan.py

---
+++ Section_7/in_n_so_chan.py
@@ -0,0 +1,12 @@
+# Viết chương trình cho phép người dùng nhập vào một số nguyên dương N. Sử dụng vòng lặp while để in ra tất cả các số chẵn từ 1 đến N.
+
+n = input("Nhập n: ")
+if n.isdigit():
+    x = int(n)
+    if x == 1:
+        print("Không có số nào")
+    else:
+        i = 2
+        while(i <= x):
+            print(i, end = " ")
+            i+=2
